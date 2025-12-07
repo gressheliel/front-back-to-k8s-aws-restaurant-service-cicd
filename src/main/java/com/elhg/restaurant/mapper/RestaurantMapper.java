decacbcbbd00320ae9b11a61f026e0df4a3f2ef5
Eliel Herrera Gress <gressheliel@gmail.com>
2025-12-06 19:05:05 -0600

    First commit

---
+++ src/main/java/com/elhg/restaurant/mapper/RestaurantMapper.java
@@ -0,0 +1,17 @@
+package com.elhg.restaurant.mapper;
+
+import com.elhg.restaurant.dto.RestaurantDTO;
+import com.elhg.restaurant.entity.Restaurant;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface RestaurantMapper {
+
+    RestaurantMapper INSTANCE = Mappers.getMapper(RestaurantMapper.class);
+
+    Restaurant mapRestaurantDTOToRestaurant(RestaurantDTO restaurantDTO);
+
+    RestaurantDTO mapRestaurantToRestaurantDTO(Restaurant restaurant);
+
+}
---
+++ src/main/java/com/elhg/restaurant/repository/RestaurantRepository.java
@@ -0,0 +1,9 @@
+package com.elhg.restaurant.repository;
+
+import com.elhg.restaurant.entity.Restaurant;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RestaurantRepository extends JpaRepository<Restaurant, Integer> {
+}
